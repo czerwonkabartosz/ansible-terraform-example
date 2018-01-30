3a1463e6870870223f57e93d90c940e2ef4899e6
Bartosz Czerwonka <b.czerwonka@cksource.com>
2018-01-30 16:02:13 +0100

    Added default to terraform vars

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,9 +2,13 @@ variable "aws_access_key" {}
 
 variable "aws_secret_key" {}
 
-variable "aws_region" {}
+variable "aws_region" {
+  default = "eu-west-1"
+}
 
-variable "public_key_path" {}
+variable "public_key_path" {
+  default = "~/.ssh/id_rsa.pub"
+}
 
 terraform {
   backend "s3" {
